30743fe355044c4e2b27e239ebe115490392a2de
maxinelobato <barretolobato.org@gmail.com>
2023-05-03 17:57:11 -0300

    changes

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -4,12 +4,12 @@ import { ButtonHero } from "../ButtonHero";
 export function Hero(){
     return(
     <>
-    <div className="hero min-h-screen bg-[url('/img/fernandaromano.jpg')] md:bg-['/img/fernandaromano2.jpg'] xl:bg-[url('/img/fernandaromano2.jpg')]">
+    <div className="hero min-h-min bg-[url('/img/fernandaromano.jpg')] md:bg-['/img/fernandaromano2.jpg'] xl:bg-[url('/img/fernandaromano2.jpg')]">
         <div className="hero-overlay bg-gradient-to-r from-[#2F1813]/60 bg-opacity-25">
         <header>
     <nav className="flex justify-center pb-20 mx-auto max-w-screen-xl sm:px-8 sm:flex sm:space-x-6">
-        <a href="/">
-            <Image
+     <a href="/" className="drop-shadow-lg">
+        <Image
             src="/img/logo-branca.png" 
             width={300} 
             height={50}
@@ -23,7 +23,7 @@ export function Hero(){
                 <div className="space-y-10 max-w-xl">
                 <div className="card min-w-full w-full h-auto backdrop-blur-sm bg-white/5 shadow-lg">
                 <div className="card-body">
-                <h1 className="text-4xl drop-shadow-lg text-gray-100 font-extrabold mx-auto md:text-5xl">
+                <h1 className="text-3xl drop-shadow-lg text-gray-100 font-extrabold mx-auto md:text-4xl">
                       Buscando uma Advogada  <span className="text-transparent bg-clip-text bg-[#2C1718]">especialista em Direito de Família?</span>
                     </h1>
                     <h2 className="pt-4 pb-4 max-w-2xl text-lg drop-shadow-lg text-gray-200 mx-auto">

50da63517a533f5414f0ee2c2e576ac7d641ade7
maxinelobato <barretolobato.org@gmail.com>
2023-05-08 23:28:52 -0300

    changes

--- src/app/components/Faq/index.tsx
+++ src/app/components/Faq/index.tsx
@@ -1,84 +1,92 @@
-export function Faq(){
-
-    const faqsList = [
-        {
-            q: "What are some random questions to ask?",
-            a: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question."
-        },
-        {
-            q: "Do you include common questions?",
-            a: "This generator doesn't include most common questions. The thought is that you can come up with common questions on your own so most of the questions in this generator."
-        },
-        {
-            q: "Can I use this for 21 questions?",
-            a: "Yes! there are two ways that you can use this question generator depending on what you're after. You can indicate that you want 21 questions generated."
-        },
-        {
-            q: "Are these questions for girls or for boys?",
-            a: "The questions in this generator are gender neutral and can be used to ask either male of females (or any other gender the person identifies with)."
-        },
-        {
-            q: "What do you wish you had more talent doing?",
-            a: "If you've been searching for a way to get random questions, you've landed on the correct webpage. We created the Random Question Generator to ask you as many random questions as your heart desires."
-        }
-    ]
-
-    return(
-        <>
-        <section className="py-14">
-            <div className="max-w-screen-xl mx-auto px-4 text-center text-white md:px-8">
-                <div className="max-w-5xl mx-auto">
-                <h1 className="text-4xl drop-shadow-lg text-white font-extrabold mx-auto md:text-5xl mb-4">
-                Perguntas e respostas que    
+export function Faq() {
+  return (
+    <>
+      <section className="py-14">
+        <div className="max-w-screen-lg mx-auto p-8 text-center text-white md:px-8">
+          <div className="max-w-5xl mx-auto">
+            <h1 className="text-4xl drop-shadow-lg text-white font-extrabold mx-auto md:text-5xl mb-4">
+              Perguntas e respostas que
             </h1>
             <span className="before:block before:rounded-xl before:absolute before:-inset-1 before:-skew-y-0 p-2 text-2xl drop-shadow-lg text-gray-300 font-extrabold mx-auto md:text-4xl before:bg-gradient-to-r from-rose-700 to-rose-950 relative inline-block">
-        <span className="relative text-white">as mães, mais perguntam</span>
-        </span>
+              <span className="relative text-white">as mães, mais perguntam</span>
+            </span>
+          </div>
+          <div className="mt-14 max-w-3xl mx-auto">
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4" open>
+              <summary className="text-xl text-white font-semibold select-none">
+                Na guarda compartilhada existe o pagamento da pensão alimentícia?
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Sim</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+                Preciso de autorização do pai, para me mudar de Cidade?
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Sim</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+                Somente meu marido pagou a casa, esse casa pode ser partilhada?
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Pesgunta 3</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+                O meu ex marido precisa aceitar o divórcio ? Ou pode o juiz já dar?
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Pesgunta 4</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+                Morei com meu namorado, isso configura união estável?
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Sim! Sendo a convivência pública, contínua e duradoura e estabelecida com o objetivo de constituição de família.</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+              É preciso ser rico para adotar?
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Não, a pessoa não precisa ter casa própria e nem ser rica.</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+              Qual o salário necessário para poder se adotar? 
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>O suficiente para que se tenha as condições necessárias para dar subsistência e educação adequada a uma criança ou adolescente.</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+              Posso pedir a visita assistida, do meu filho recém nascido?  
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Sim, nesta idade é possível solicitar que as visitas do pai ao filho, ocorram na sua casa! Fazendo um período de adaptação.</p>
+              </div>
+            </details>
+            <details className="mt-2 text-left bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-4">
+              <summary className="text-xl text-white font-semibold select-none">
+              Posso pedir revisão da pensão alimentícia em qualquer momento?  
+              </summary>
+              <div className="p-4 mt-3 text-lg text-white">
+                <p>Sim! Você pode pedir a revisão da pensão, sendo que as necessidades do seu filho (a) tenham aumentado ou o pai tenha tido um aumento em sua capacidade financeira.</p>
+              </div>
+            </details>  
+          </div>
         </div>
-        <div className="mt-14 max-w-2xl mx-auto">
-          <details className="mt-2 bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-2" open>
-    <summary className="text-xl text-white font-semibold select-none">
-      Pesgunta 1
-    </summary>
-    <div className="mt-3 text-lg text-white">
-      <p>Pesgunta 1</p>
-    </div>
-  </details>
-  <details className="mt-2 bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-2">
-    <summary className="text-xl text-white font-semibold select-none">
-      Pesgunta 2
-    </summary>
-    <div className="mt-3 text-lg text-white">
-      <p>Pesgunta 2</p>
-    </div>
-  </details>
-  <details className="mt-2 bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-2">
-    <summary className="text-xl text-white font-semibold select-none">
-      Pesgunta 3
-    </summary>
-    <div className="mt-3 text-lg text-white">
-      <p>Pesgunta 3</p>
-    </div>
-  </details>
-  <details className="mt-2 bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-2">
-    <summary className="text-xl text-white font-semibold select-none">
-      Pesgunta 4
-    </summary>
-    <div className="mt-3 text-lg text-white">
-      <p>Pesgunta 4</p>
-    </div>
-  </details>
-  <details className="mt-2 bg-rose-950 rounded-xl ring-1 ring-slate-900/5 shadow-lg hover:bg-rose-950/30 hover:transition-all hover:ease-out hover:duration-300 hover:ring-2 hover:ring-white open:ring-1 open:ring-black/5 open:shadow-lg p-2">
-    <summary className="text-xl text-white font-semibold select-none">
-      Pesgunta 5
-    </summary>
-    <div className="mt-3 text-lg text-white">
-      <p>Pesgunta 5</p>
-    </div>
-  </details>
-</div>
-        </div>
-</section>
-        </>
-    )
+      </section>
+    </>
+  )
 }

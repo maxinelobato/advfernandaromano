d7a867d875f1911ab5d6132212eec5b525497237
maxinelobato <barretolobato.org@gmail.com>
2023-05-10 17:15:34 -0300

    googletagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import './globals.css'
 import { Gruppo } from 'next/font/google'
+import Script from 'next/script'
 
 const gruppo = Gruppo({ 
   weight: ['400'],
@@ -21,8 +22,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br" className={gruppo.className}>
+        {/* Google Tag Manager - Global base code */}
+        <Script
+        id="gtag-base"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PCQ5JSR');
+          `,
+        }}
+      />
        <Suspense>
-      <body className='bg-gradient-to-tr from-rose-400 to-rose-950'>{children}</body>
+      <body className='bg-gradient-to-tr from-rose-400 to-rose-950'>
+        {children}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-PCQ5JSR`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+      </body>
       </Suspense>
     </html>
   )

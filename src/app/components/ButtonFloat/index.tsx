4e7aceee3cfad37f567ec738082a6df88591e49b
maxinelobato <barretolobato.org@gmail.com>
2023-05-22 00:07:16 -0300

    changes

--- src/app/components/ButtonFloat/index.tsx
+++ src/app/components/ButtonFloat/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import WhatsappIcon from '../icons/whatsappicon'
 export default function ButtonFloat () {
   return (
     <>
@@ -12,15 +13,7 @@ export default function ButtonFloat () {
           target='_blank'
         >
           <button className='z-[100] fixed bottom-3 right-8 pl-2 w-12 h-12 bg-[#25D366] rounded-full hover:bg-green-800 active:shadow-lg mouse shadow-lg transition ease-in duration-200 focus:outline-none'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='32'
-              height='32'
-              fill='#d9d9d9'
-              viewBox='0 0 256 256'
-            >
-              <path d='M152.58,145.23l23,11.48A24,24,0,0,1,152,176a72.08,72.08,0,0,1-72-72A24,24,0,0,1,99.29,80.46l11.48,23L101,118a8,8,0,0,0-.73,7.51,56.47,56.47,0,0,0,30.15,30.15A8,8,0,0,0,138,155ZM232,128A104,104,0,0,1,79.12,219.82L45.07,231.17a16,16,0,0,1-20.24-20.24l11.35-34.05A104,104,0,1,1,232,128Zm-40,24a8,8,0,0,0-4.42-7.16l-32-16a8,8,0,0,0-8,.5l-14.69,9.8a40.55,40.55,0,0,1-16-16l9.8-14.69a8,8,0,0,0,.5-8l-16-32A8,8,0,0,0,104,64a40,40,0,0,0-40,40,88.1,88.1,0,0,0,88,88A40,40,0,0,0,192,152Z'></path>
-            </svg>
+            <WhatsappIcon />
           </button>
         </Link>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,30 +2,32 @@ import Script from 'next/script'
 import './globals.css'
 import { Gruppo } from 'next/font/google'
 import { GTM_ID } from '../../lib/gtm'
+import { Suspense } from 'react'
 
-const gruppo = Gruppo({ 
+const gruppo = Gruppo({
   weight: ['400'],
   style: ['normal'],
   subsets: ['latin'],
-  display: 'swap',
+  display: 'swap'
 })
 
 export const metadata = {
   title: 'Fernanda Romano Advocacia - Especialista em Direito de Família',
-  description: 'Pensão Alimentícia, Divórcio, Regulamentção de Visita e Partilha de Bens',
+  description:
+    'Pensão Alimentícia, Divórcio, Regulamentção de Visita e Partilha de Bens'
 }
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/* Google Tag Manager - Global base code */}
       <Script
-        id="gtag-base"
-        strategy="afterInteractive"
+        id='gtag-base'
+        strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -33,18 +35,19 @@ export default function RootLayout({
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', 'GTM-PCQ5JSR');
-          `,
-        }}></Script>
+          `
+        }}
+      ></Script>
       <body className={gruppo.className}>
-        {children}
+        <Suspense><main>{children}</main></Suspense>
         <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
       </body>
     </html>
   )

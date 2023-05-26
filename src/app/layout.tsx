ba92965d18876a897e6a0cfa5bf10ff6975dcf32
maxinelobato <barretolobato.org@gmail.com>
2023-05-26 16:51:15 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import Script from 'next/script'
 import './globals.css'
 import { Gruppo } from 'next/font/google'
-import { GTM_ID } from '../../lib/gtm'
 import { Suspense } from 'react'
+import Analytics from './components/Analytics'
 
 const gruppo = Gruppo({
   weight: ['400'],
@@ -24,30 +24,13 @@ export default function RootLayout ({
 }) {
   return (
     <html lang='en'>
-      {/* Google Tag Manager - Global base code */}
-      <Script
-        id='gtag-base'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', 'GTM-PCQ5JSR');
-          `
-        }}
-      ></Script>
       <body className={gruppo.className}>
-        <Suspense><main>{children}</main></Suspense>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height='0'
-            width='0'
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
+        <Suspense>
+          <main>{children}</main>
+          <Suspense>
+            <Analytics />
+          </Suspense>
+        </Suspense>
       </body>
     </html>
   )

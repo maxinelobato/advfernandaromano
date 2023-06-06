e45f7aa4c530957f427dc4e12e79387718dc8d3f
maxinelobato <barretolobato.org@gmail.com>
2023-06-06 14:27:10 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
-'use client'
-
 import Script from 'next/script'
 import './globals.css'
 import { Gruppo } from 'next/font/google'
-import { GTM_ID } from '../../lib/gtm'
 import { Suspense } from 'react'
 
 const gruppo = Gruppo({
@@ -28,31 +25,33 @@ export default function RootLayout ({
     <html lang='en'>
       {/* Google Tag Manager - Global base code */}
       <Suspense>
-      <Script
-        id='gtag-base'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
+        <Script
+          id='gtag-base'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', 'GTM-PCQ5JSR');
           `
-        }}
-      ></Script>
+          }}
+        ></Script>
       </Suspense>
       <body className={gruppo.className}>
-        <Suspense><main>{children}</main></Suspense>
         <Suspense>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height='0'
-            width='0'
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
+          <main>{children}</main>
+        </Suspense>
+        <Suspense>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-PCQ5JSR`}
+              height='0'
+              width='0'
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
         </Suspense>
       </body>
     </html>
